38ca48b0559fa5648fb5831a6911306815097615
Scott Weldon <scottw@scvdata.com>
2015-06-08 10:58:06 -0700

    Fix hook to only run if scenario has passed

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,9 +1,11 @@
 After do |scenario|
-  visit root_path
-  ["apple", "pear"].each do |wiki|
-    id = "delete_wiki-#{wiki}"
-    unless first(:css, "##{id}").nil?
-      click_on "#{id}"
+  if scenario.passed?
+    visit root_path
+    ["apple", "pear"].each do |wiki|
+      id = "delete_wiki-#{wiki}"
+      unless first(:css, "##{id}").nil?
+        click_on "#{id}"
+      end
     end
   end
 end
